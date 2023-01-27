3070bf78e4a8533dfe49d57c79ee2f3227615196
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-01-27 17:23:37 +0100

    feat: add 404 page

--- src/router/AppRotuer.jsx
+++ src/router/AppRotuer.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Plp } from "../pages/Plp";
 import { Pdp } from "../pages/Pdp";
 import { SearchPage } from "../pages/SearchPage";
+import { NotFound404 } from "../pages/NotFound404";
 
 export const AppRotuer = () => {
   return (
@@ -11,6 +12,7 @@ export const AppRotuer = () => {
         <Route path="/phone_solutions" element={<Plp />} />
         <Route path="/phone_solutions/:id" element={<Pdp />} />
         <Route path="/phone_solutions" element={<SearchPage />} />
+        <Route path="*" element={<NotFound404 />} />
       </Routes>
     </>
   );
